3f0b08f162600edfe631ef79ba81b3810b9b6b91
Abd-afeez Abd-hamid <dent4real@yahoo.com>
2019-02-07 09:36:38 +0000

    feature: UI landing page

---
+++ js/app.js
@@ -0,0 +1,72 @@
+  var status = 0,
+    currentModal = '';
+  // algorithm function for homepage animated text
+  var animateText = function() {
+    switch (status) {
+      case '0':
+        document.getElementById('text-anim').innerText = 'We deliver meals to your doorstep';
+        document.getElementById('jumbo').classList.remove('bg');
+        document.getElementById('jumbo').classList.add('bg2');
+        status = 1;
+        break;
+      case '1':
+        document.getElementById('text-anim').innerText = 'Hot and spicy any day anytime';
+        document.getElementById('jumbo').classList.remove('bg2');
+        document.getElementById('jumbo').classList.add('bg3');
+        status = 2;
+        break;
+      case '2':
+        document.getElementById('text-anim').innerText = 'Option to pay on delivery or up front';
+        document.getElementById('jumbo').classList.remove('bg3');
+        document.getElementById('jumbo').classList.add('bg4');
+        status = 3;
+        break;
+      case '3':
+        document.getElementById('text-anim').innerText = 'Meals are affordable and hygienically prepared';
+        document.getElementById('jumbo').classList.remove('bg4');
+        document.getElementById('jumbo').classList.add('bg');
+        status = 0;
+        break;
+      default:
+        document.getElementById('text-anim').innerText = 'Delicious meals you can\'t resist';
+        document.getElementById('jumbo').classList.remove('bg5');
+        document.getElementById('jumbo').classList.add('bg');
+        status = 0;
+    }
+  },
+    // function for toggling login and signup modal
+    toggleModal = function(e) {
+      const elem = e.target.getAttribute('data-modal');
+      if (currentModal && currentModal !== elem) {
+        document.getElementById(currentModal).classList.add('hidden');
+        document.getElementById(elem).classList.remove('hidden');
+      } else {
+        document.getElementById(elem).classList.toggle('hidden');
+      }
+      currentModal = elem;
+    },
+  
+    // function for dismissing modal
+    dismissModal = function() {
+      if (currentModal) {
+        document.getElementById(currentModal).classList.add('hidden');
+        currentModal = null;
+      }
+    },
+  // function for page animation and modal script
+  startAnimation = function() {
+    const startAnim = setInterval(animateText, 3000);
+    const classname = document.getElementsByClassName('trigger');
+    Array.from(classname).forEach( function(element) {
+      element.addEventListener('click', toggleModal);
+    });
+    const dismissname = document.getElementsByClassName('dismiss');
+    Array.from(dismissname).forEach( function(element) {
+      element.addEventListener('click', dismissModal);
+    });
+  };
+  // onload methods for ui animation and signup and login modal events
+window.onload = () => {
+  // checkIt();
+  startAnimation();
+};
